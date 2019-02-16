663c5f17c123cb7067e15c9f0a5f4c20b1e68b3b
dsli <ds.li@outlook.com>
2019-02-16 13:56:12 -0500

    Update file directories for Warm Up Project 1

---
+++ app/routing.py
@@ -0,0 +1,22 @@
+from flask import render_template, request, Blueprint
+import time
+# from flaskr.db import get_db
+# from app import app
+from app import ttt_app
+
+ttt = Blueprint('ttt', __name__, url_prefix='/ttt')
+
+@ttt_app.route('/', methods=['POST'], endpoint='/ttt/play/')
+@ttt_app.route('/ttt/', methods=['POST'], endpoint='/ttt/play/')
+
+def get_name():
+    if request.method == 'POST':
+        name = request.form['name']
+        if not name:
+            error = 'Name is required.'
+
+        return greeting(name)
+
+
+def greeting(name):
+    return "Hello" + name + "," + time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.gmtime())
