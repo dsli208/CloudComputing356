3b2e27c3ed3838e0c420f4de47e3bd526ee60b2d
dsli <ds.li@outlook.com>
2019-02-19 00:02:03 -0500

    Adding base code for REST portion

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, jsonify
 import time
+from flask_restful import Api, Resource, reqparse
 # from app.ttt_bp import ttt_bp
 
 ttt_app = Flask(__name__)
+
+ttt_props = {"grid": [' ', ' ', ' ' , ' ', ' ', ' ', ' ', ' ', ' '], "winner": ' '}
 
 # ttt_app.register_blueprint(ttt_bp, url_prefix='/ttt')
 
@@ -16,5 +19,23 @@ def index():
     else:
         return render_template('hw1.html', name=None, date=date)
 
+@ttt_app.route('/ttt/play', methods=['POST', 'GET'])
+def get_board():
+    return jsonify(ttt_props)
+
+class Game(Resource):
+    def get(self, name):
+        pass
+
+    def post(self, name):
+        pass
+
+    def put(self, name):
+        pass
+
+    def delete(self, name):
+        pass
+
 if __name__ == '__main__':
-   ttt_app.run(debug = True, host='0.0.0.0', port=80)
+    ttt_app.run(debug=True)
+    #ttt_app.run(debug = True, host='0.0.0.0', port=80)
--- app/tic_tac_toe_play.py
+++ app/tic_tac_toe_play.py
@@ -1,3 +1,6 @@
+from flask import Flask
+# from flask_restful import Api, Resource, reqparse
+
 import json
 
 x = {"grid": [' ', ' ', ' ' , ' ', ' ', ' ', ' ', ' ', ' '], "winner": ' '}
