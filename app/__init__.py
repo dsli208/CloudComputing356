1a1e73d01b572e0fb294e79801889644cc2630d9
dsli <ds.li@outlook.com>
2019-02-28 00:11:06 -0500

    Construct verify page

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,23 @@
+import pymongo
 from random import randint
 import json
 
-from flask import Flask, render_template, request, url_for, jsonify
-import time
+from flask import Flask, render_template, request, url_for, jsonify, redirect
+from flask_mail import Mail, Message
+from flask_pymongo import PyMongo
 
 ttt_app = Flask(__name__)
 ttt_props = {'name': '', 'date' : '', 'grid': [' ', ' ', ' ' , ' ', ' ', ' ', ' ', ' ', ' '], 'winner': ' '}
 move_id = 0
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+db = client["db"]
+users = db['users']
+games = db['games']
+keys = db['keys']
 
 def props_clear():
     global ttt_props
     ttt_props = {'name': '', 'date' : '', 'grid': [' ', ' ', ' ' , ' ', ' ', ' ', ' ', ' ', ' '], 'winner': ' '}
-    # print(ttt_props)
-    # print("Props cleared")
 
 def computer_play():
     global ttt_props
@@ -43,28 +48,53 @@ def is_winner(player):
     elif ttt_props['grid'][2] == ttt_props['grid'][4] == ttt_props['grid'][6] == player:
         return True
 
-# ttt_app.register_blueprint(ttt_bp, url_prefix='/ttt')
-@ttt_app.route('/', methods=['GET', 'POST'])
-@ttt_app.route('/ttt/', methods=['GET', 'POST'])
-@ttt_app.route('/ttt', methods=['GET', 'POST'])
+# Default route, login
+@ttt_app.route('/login')
+def login():
+    pass
+
+# Add user
+@ttt_app.route('/adduser', methods=['GET', 'POST'])
 def index():
     global move_id
     move_id += 1
     global ttt_props
-    date = time.strftime("%Y-%m-%d", time.gmtime())
     if request.method == 'POST':
-        # print('POST request')
+        # Gather the details and add to users DB
         name = request.form['name']
         ttt_props['name'] = name
-        ttt_props['date'] = date
-        ttt_grid = json.dumps(ttt_props)
-        return render_template('hw1.html', name=ttt_props['name'], date=date, winner=None, board=ttt_grid, getupdate=False, id=move_id)
+        mail_addr = request.form['email']
+        userinfo = {'username': name, 'password': request.form['password'], 'email': mail_addr}
+        # users.insert_one(userinfo)
+
+        # Send the message
+        mail = Mail(ttt_app)
+        msg = Message("Tic-Tac-Toe Registration", sender="dsli@tictactoe.com")
+        key = "abracadabra"  # temporary string until we can generate something random
+        msg.body = "The key is: " + key
+        msg.add_recipient(mail_addr)
+        mail.send(msg)
+
+        # Get the email-key pair and add it to the keys DB
+        keypair = {'email': mail_addr, 'key': key}
+        # keys.insert_one(keypair)
+
+        # return redirect
+        return redirect("/verify", code=302)
     else:
         props_clear()
-        # print('GET request')
         ttt_grid = json.dumps(ttt_props)
-        # print(ttt_props)
-        return render_template('hw1.html', name=None, date=None, winner=None, board=ttt_grid, getupdate=False, id=move_id)
+        return render_template('hw1.html', name=None, winner=None, email=None, board=ttt_grid, getupdate=False, id=move_id)
+
+# Verify user, send message with key
+@ttt_app.route('/verify')
+def send_verification():
+    if request.method == 'POST':
+        email_addr = request.form['email']
+        return render_template('hw1verify.html')
+    else:
+        return render_template('hw1verify.html')
+
 
 @ttt_app.route('/ttt/play/', methods=['POST', 'GET'])
 @ttt_app.route('/ttt/play', methods=['POST', 'GET'])
@@ -72,11 +102,8 @@ def board():
     global move_id
     global ttt_props
     move_id += 1
-    # print('Request JSON')
 
     if request.method == 'POST':
-        # print(request.json)
-        # space = int(request.json['grid_id'])
         print("request grid")
         print(request.json['grid'])
         ttt_props['grid'] = request.json['grid']
@@ -102,55 +129,11 @@ def board():
                 jify = jsonify(ttt_props)
                 print(jify)
                 return jify
-    # return render_template('hw1.html', name=ttt_props['name'], date=ttt_props['date'], winner='O', board=ttt_grid, getupdate=True, id=move_id)
-    #        else:
-    #            ttt_grid = json.dumps(ttt_props)
-    # print(ttt_props)
-    #            jify = jsonify(ttt_props)
-    # print(jify)
-    #            return jify
-        # if ttt_props['grid'][space] == ' ':
-        #    ttt_props['grid'][space] = 'X'
-        #    if is_winner('X'):
-        #        ttt_props['winner'] = 'X'
-        #        ttt_grid = json.dumps(ttt_props)
-                # print(ttt_props)
-        #        jify = jsonify(ttt_props)
-                # print(jify)
-        #        return jify
-                #return render_template('hw1.html', name=ttt_props['name'], date=ttt_props['date'], winner='X', board=ttt_grid, getupdate=True, id=move_id)
-        #    else:
-        #        computer_play()
-        #        if is_winner('O'):
-        #            ttt_props['winner'] = 'O'
-        #            ttt_grid = json.dumps(ttt_props)
-                    # print(ttt_props)
-        #            jify = jsonify(ttt_props)
-                    # print(jify)
-        #            return jify
-                    # return render_template('hw1.html', name=ttt_props['name'], date=ttt_props['date'], winner='O', board=ttt_grid, getupdate=True, id=move_id)
-        #        else:
-        #            ttt_grid = json.dumps(ttt_props)
-                    # print(ttt_props)
-        #            jify = jsonify(ttt_props)
-                    # print(jify)
-        #            return jify
-                    # return render_template('hw1.html', name=ttt_props['name'], date=ttt_props['date'], winner=None, board=ttt_grid, getupdate=True, id=move_id)
-        #else:
-        #    ttt_grid = json.dumps(ttt_props)
-            # print(ttt_grid)
-        #    jify = jsonify(ttt_props)
-            # print(jify)
-        #    return jify
-            # return render_template('hw1.html', name=ttt_props['name'], date=ttt_props['date'], winner=None, board=ttt_grid, getupdate=False, id=move_id)
     else:
         ttt_grid = json.dumps(ttt_props)
-        # print(ttt_props)
         jify = jsonify(ttt_props)
-        # print(jify)
         return jify
-        # return render_template('hw1.html', name=ttt_props['name'], date=ttt_props['date'], winner=None, board=ttt_grid, getupdate=False, id=move_id)
 
 if __name__ == '__main__':
     ttt_app.run(debug=True)
-    #ttt_app.run(debug = True, host='0.0.0.0', port=80)
+    # ttt_app.run(debug = True, host='0.0.0.0', port=80)
