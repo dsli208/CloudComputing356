2e46fa53cdba240ff33b272acffba493bd349a61
dsli <ds.li@outlook.com>
2019-03-04 17:52:34 -0500

    Fix bug in computer win for inserting ttt_props

--- app/__init__.py
+++ app/__init__.py
@@ -351,7 +351,7 @@ def board():
                 username = session['username']
                 game_info_db = games.find_one({"username": username})
                 start_date = time.strftime("%Y-%m-%d", time.gmtime())
-                saved_game_data = {"id": game_info_db['id'], "start_date": start_date} # put props key in here?
+                saved_game_data = {"id": game_info_db['id'], "start_date": start_date, "props": ttt_props} # put props key in here?
                 # Append game data to past games array
                 game_info_db['game_list'].append(saved_game_data)
                 # Update ID
